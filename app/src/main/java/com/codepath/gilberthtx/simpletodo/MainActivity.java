9f89a62ce78866511b4bda0fde26eb5728642d05
Gilbert <44419585+Gilberthtx@users.noreply.github.com>
2021-08-16 13:33:10 -0500

    UI setup

---
+++ app/src/main/java/com/codepath/gilberthtx/simpletodo/MainActivity.java
@@ -0,0 +1,24 @@
+package com.codepath.gilberthtx.simpletodo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    List<String> items;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        items = new ArrayList<>();
+        items.add("Buy milk");
+        items.add("Go to the gym");
+        items.add("Have fun!");
+    }
+}
