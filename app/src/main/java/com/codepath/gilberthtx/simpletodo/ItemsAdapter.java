a7bdba17c99246be130d2e7dc30e8f35ddff84e8
Gilbert <44419585+Gilberthtx@users.noreply.github.com>
2021-08-16 21:46:14 -0500

    Add and remove items + data persistence
    You can now add and remove items from your to-do list and the app will save them and load the items on app start. Using FileUtils class from Apache Common IO 2.6

--- app/src/main/java/com/codepath/gilberthtx/simpletodo/ItemsAdapter.java
+++ app/src/main/java/com/codepath/gilberthtx/simpletodo/ItemsAdapter.java
@@ -10,13 +10,19 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 
-//Displays data from model into row in recycler view
+// Displays data from model into row in recycler view
 public class ItemsAdapter extends RecyclerView.Adapter<ItemsAdapter.ViewHolder>{
 
-    List <String> items;
+    public interface OnLongClickListener {
+        void onItemLongClicked(int position);
+    }
 
-    public ItemsAdapter(List<String> items) {
+    List <String> items;
+    OnLongClickListener longClickListener;
+
+    public ItemsAdapter(List<String> items, OnLongClickListener longClickListener) {
         this.items = items;
+        this.longClickListener = longClickListener;
     }
 
     @NonNull
@@ -24,11 +30,11 @@ public class ItemsAdapter extends RecyclerView.Adapter<ItemsAdapter.ViewHolder>{
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         // Use layout inflater to inflate view
         View todoView = LayoutInflater.from(parent.getContext()).inflate(android.R.layout.simple_list_item_1, parent, false);
-        //wrap it inside a view holder and return it
+        // wrap it inside a view holder and return it
         return new ViewHolder(todoView);
     }
 
-    //binds data to a particular view holder
+    // binds data to a particular view holder
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         // grab item at position
@@ -37,14 +43,14 @@ public class ItemsAdapter extends RecyclerView.Adapter<ItemsAdapter.ViewHolder>{
         holder.bind(item);
     }
 
-    //Tells recycler view how many items are in the list
+    // Tells recycler view how many items are in the list
     @Override
     public int getItemCount() {
         return items.size();
 
     }
 
-    //Container to provide easy access to views that represent each row of the list
+    // Container to provide easy access to views that represent each row of the list
     class ViewHolder extends RecyclerView.ViewHolder {
 
         TextView tvItem;
@@ -57,6 +63,14 @@ public class ItemsAdapter extends RecyclerView.Adapter<ItemsAdapter.ViewHolder>{
         // update the view inside of the view holder with this data
         public void bind(String item) {
             tvItem.setText(item);
+            tvItem.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    // notify listener which position was long clicked
+                    longClickListener.onItemLongClicked(getAdapterPosition());
+                    return true;
+                }
+            });
         }
     }
 }
